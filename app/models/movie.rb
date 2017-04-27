07f25326c306d80e3a7e772706113f463a1319dd
Mykola Matsko <mykola.matsko@gmail.com>
2017-04-27 17:35:13 +0300

    Add PaperClip

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ActiveRecord::Base
+	has_attached_file :image, styles: { medium: "400x600#" }
+	validates :image, attachment_presence: true
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170427135129) do
+ActiveRecord::Schema.define(version: 20170427141558) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -23,6 +23,10 @@ ActiveRecord::Schema.define(version: 20170427135129) do
     t.boolean  "want"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
